97438c04b8314b6f31fb019581d0ac0d2f9a1fc3
vhskishore <hemasivakishore@hotmail.com>
2024-04-21 14:55:01 +0530

    Testing

--- eks.tf
+++ eks.tf
@@ -4,7 +4,7 @@ module "eks" {
   cluster_version                = "1.29"
   cluster_endpoint_public_access = true
   vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = var.private_subnets
+  subnet_ids                     = module.vpc.private_subnets
   eks_managed_node_groups = {
     nodes = {
       min_size      = 1
