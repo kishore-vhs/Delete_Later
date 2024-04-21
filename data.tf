ac2fa46c9a3221c9f36d5474e0fbf76461b20b3e
vhskishore <hemasivakishore@hotmail.com>
2024-04-21 15:41:40 +0530

    test

--- data.tf
+++ data.tf
@@ -6,7 +6,7 @@ data "aws_availability_zones" "azs" {}
 # }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
